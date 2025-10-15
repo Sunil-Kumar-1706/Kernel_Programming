ae266eee7597955eaee8a940244cc47cf4a8d924
SUNIL KUMAR <v24ce1@gmail.com>
2025-10-15 10:48:15 +0530

    Kernel Programming

---
+++ Character_Device_Driver/03_IOCTL/1_ioctl/blksize.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+
+int main()
+{
+	int fd;
+	unsigned long num_blocks;
+
+	fd = open("/dev/sda1", O_RDONLY);
+	perror("fd");
+
+	//0x00001260   BLKGETSIZE            unsigned long *
+	ioctl(fd, 0x00001260,	&num_blocks);
+	perror("ioctl");
+	printf("Number of blocks: %lu, this makes %.3f GB\n",
+	 num_blocks,
+	 (double)num_blocks * 512.0 / (1024 * 1024 * 1024));
+	close(fd);
+	return 0;
+}
